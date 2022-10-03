046b1f4156d52198edaece7fd7b61ea3af179453
dangkhoa2022 <pdkhoa137@gmail.com>
2022-10-03 19:00:50 +0700

    khoa practice

---
+++ main.js
@@ -0,0 +1,6 @@
+//Bài 2: Viết một đoạn mã Javascript, khai báo biến width chứa giá trị độ rộng của hình chữ nhật, biến height chứa giá trị chiều cao của hình chữ nhật. In ra màn hình diện tích của hình chữ nhật đó.
+
+
+let width = 20
+let height = 10
+document.write('S = ', S = (width + height)*2)
